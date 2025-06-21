ee522be891491763083530485fce9e8a77d06a5d
NVDIArm <tanawatvejchaboot@gmail.com>
2025-06-21 10:12:31 +0700

    step 9: Listview widget

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -11,47 +11,73 @@ class Item extends StatefulWidget {
 class _ItemState extends State<Item> {
   int quantity = 10;
 
-void addQuantity() {
-    setState(() {
-      quantity++;
-    });
-  }
+// void addQuantity() {
+//     setState(() {
+//       quantity++;
+//     });
+//   }
 
-void subtractQuantity() {
-    setState(() {
-      if (quantity > 0) {
-        quantity--;
-      }
-    });
-  }
+// void subtractQuantity() {
+//     setState(() {
+//       if (quantity > 0) {
+//         quantity--;
+//       }
+//     });
+//   }
+
+// step 9: Listview widget
+List data = ["Isak", "Wirtz", "Kerkaze", "Frimpong", "Mamardashvili"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-          Text(
-            "Quantity: $quantity",
-            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    // return Center(
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //      children: [
+    //       Text(
+    //         "Quantity: $quantity",
+    //         style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    //       ),
+    //       const SizedBox(height: 20),
+    //       Row(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: [
+    //           ElevatedButton(
+    //             onPressed: addQuantity,
+    //             child: const Text("Add"),
+    //           ),
+    //           const SizedBox(width: 20),
+    //           ElevatedButton(
+    //             onPressed: subtractQuantity,
+    //             child: const Text("Subtract"),
+    //           ),
+    //         ],
+    //       ),
+    //      ],
+    //   ),
+    // );
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10),
           ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: addQuantity,
-                child: const Text("Add"),
-              ),
-              const SizedBox(width: 20),
-              ElevatedButton(
-                onPressed: subtractQuantity,
-                child: const Text("Subtract"),
-              ),
-            ],
+          margin: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+          padding: EdgeInsets.all(40),
+          child: Text(
+            data[index],
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-         ],
-      ),
+        );
+
+    }, 
+    itemCount: data.length
     );
   }
 }
+// step 10: Listview.builder widget
